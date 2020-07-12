46ef5e0a2422c143e66319d0ba121be0ceda406e
Matthew Francis <mattfrancis888@gmail.com>
2020-07-11 20:52:34 -0400

    Created layout for header that's responsive to small/mobile screens.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import logo from "../img/logo.png";
+import hamburger from "../img/hamburger.png";
 const Header = () => {
     return (
         <nav>
             <img src={logo} alt="pixar-logo" />
+            <img className="hamburgerIcon" src={hamburger} alt="pixar-logo" />
             <div className="headerTextsWrapper">
                 <h1>Feature Files</h1>
                 <h1>About</h1>
