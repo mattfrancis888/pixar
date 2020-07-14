5ed6973e9c4125c6d6a2eb5c6476a3d7f07d52b7
Matthew Francis <mattfrancis888@gmail.com>
2020-07-14 18:20:32 -0400

    'Featured Contents' are created in starting page. Responsive to all screens.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import pixarCareer from "../img/pixar_career.jpeg";
+import onwards from "../img/onwards.jpg";
+import soul from "../img/soul.jpg";
 const Content: React.FC<{}> = () => {
     return (
         <div>
             <div className="contentContainer">
-                <h2>
+                <h2 className="quote">
                     "Our stories often ask, what if?<br></br>
                     What if toys came to life? <br></br>
                     What if our emotions could talk? <br></br>
@@ -14,6 +16,20 @@ const Content: React.FC<{}> = () => {
                     What if the story of our future<br></br>
                     was one of liberty and justice for all?"
                 </h2>
+                <div className="featuredContentContainer">
+                    <div className="featuredContent">
+                        <img src={pixarCareer} alt="careers at pixar" />
+                        <h2 className="">Careers At Pixar</h2>
+                    </div>
+                    <div className="featuredContent">
+                        <img src={soul} alt="soul" />
+                        <h2 className="">Soul</h2>
+                    </div>
+                    <div className="featuredContent">
+                        <img src={onwards} alt="Onward" />
+                        <h2 className="">Onward Trailers</h2>
+                    </div>
+                </div>
             </div>
         </div>
     );
